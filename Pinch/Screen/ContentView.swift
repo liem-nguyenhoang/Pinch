fc06bfc71010032be5eb50fa15dc49ef51e31f10
LiemNH <liem.nguyehoang.127@gmail.com>
2022-03-28 22:02:47 +0700

    [LiemNH] Double Tap Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+        
+    @State private var isAnimation: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimation ? 1 : 0)
+                    .scaleEffect(imageScale)
+                // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    }
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)){
+                    isAnimation = true
+                }
+            }
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
