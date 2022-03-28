5360446802c2581a29d3e69d0312afdc06eaa143
LiemNH <liem.nguyehoang.127@gmail.com>
2022-03-28 21:25:21 +0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by LiemNH2 on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
